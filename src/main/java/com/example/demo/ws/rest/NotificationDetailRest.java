6b6b303f2663c28567d6679b81af1f1e541fbc8e
ayoubmazouni@gmail.com <ayoubmazouni@gmail.com>
2020-02-21 15:34:52 +0100

    Fixed Notification Rest annotation :
    Hussein is dumb.

--- src/main/java/com/example/demo/ws/rest/NotificationDetailRest.java
+++ src/main/java/com/example/demo/ws/rest/NotificationDetailRest.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Date;
 import java.util.List;
 
-@Repository
+@RestController
 @RequestMapping("/TNB-api/NotificationDetail")
 public class NotificationDetailRest {
     @Autowired
